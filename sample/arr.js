60ca2bc20e5b8d1ba587b34b73b92ff41c11b611
Siarhei Burets <siarhei_burets@epam.com>
2020-08-17 09:25:58 +0300

    remove ignored directory

--- sample/arr.js
+++
@@ -1,5 +0,0 @@
-let arr = Array.from(Array(10), (_, i) => 2 ** i);
-
-arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-console.log(arr.filter(x => x > 0 || (x > -26 && x > 26)));
--- sample/str.js
+++
@@ -1,8 +0,0 @@
-const str = 'something first blablabla second third not second';
-
-console.log(str.match(/first|(not )?second/g));
-
-let arr = [1, 2];
-
-console.log(Array.isArray(str));
-console.log(Array.isArray(arr));
